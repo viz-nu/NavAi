b38ebc6f51f1df1ed47478dcb1197bfc50fdf1e6
viz-nu <vishnu.teja101.vt@gmail.com>
2025-01-29 00:51:57 +0530

    allow all

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Your Next.js app URL
-    allow_credentials=True,
+    # allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
